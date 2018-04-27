e2ec2d03b1e3a7073a13f5056c07c515fc328c84
Jorge Fco <jorgefrac@users.noreply.github.com>
2018-04-27 11:37:34 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,12 @@
-<h1></h1>
+Email
+<?= page('contacto')->email()->html() ?>
+
+Telephone
+<?= page('contacto')->telephone()->html() ?>
+
+Phone
+<?= page('contacto')->phone()->html() ?>
+
+Whatsapp
+<?= page('contacto')->whatsapp()->html() ?>
+
