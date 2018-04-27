905da29be07d9ecf0a2cc732da88f275b5f3b3f5
Jorge Fco <jorgefrac@users.noreply.github.com>
2018-04-27 11:47:07 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -19,5 +19,27 @@ Location
 <?= page('contacto')->country()->html() ?>
 
 Social Media
-
+<ul>
+	<?php if (page('contacto')->twitter()): ?>
+	<li>
+		<a href="<?= page('contacto')->twitter() ?>" title="Twitter" target="_blank">
+	     		<?php snippet('icons/twitter') ?>
+		</a>
+	</li>
+	<?php endif ?>
+	<?php if (page('contacto')->facebook()): ?>
+	<li>
+		<a href="<?= page('contacto')->facebook() ?>" title="Facebook" target="_blank">
+			<?php snippet('icons/facebook') ?>
+		</a>
+	</li>
+	<?php endif ?>
+	<?php if (page('contacto')->instagram()): ?>
+	<li>
+		<a href="<?= page('contacto')->instagram() ?>" title="Instagram" target="_blank">
+	     		<?php snippet('icons/instagram') ?>
+		</a>
+	</li>
+	<?php endif ?>
+</ul>
 
