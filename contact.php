f1cea4dde9b2cd9ac1f7be2b182268b5bf11f004
Jorge Fco <jorgefrac@users.noreply.github.com>
2018-04-27 11:46:16 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -23,21 +23,21 @@ Social Media
 <?php if (page('contacto')->twitter()): ?>
 	<li>
 		<a href="<?= page('contacto')->twitter() ?>" title="Twitter" target="_blank">
-       			<?php snippet('icons/twitter') ?>
+       		<?php snippet('icons/twitter') ?>
 		</a>
 	</li>
 <?php endif ?>
 <?php if (page('contacto')->facebook()): ?>
 	<li>
 		<a href="<?= page('contacto')->facebook() ?>" title="Facebook" target="_blank">
-       			<?php snippet('icons/facebook') ?>
+			<?php snippet('icons/facebook') ?>
 		</a>
 	</li>
 <?php endif ?>
 <?php if (page('contacto')->instagram()): ?>
 	<li>
 		<a href="<?= page('contacto')->instagram() ?>" title="Instagram" target="_blank">
-       			<?php snippet('icons/instagram') ?>
+       		<?php snippet('icons/instagram') ?>
 		</a>
 	</li>
 <?php endif ?>
