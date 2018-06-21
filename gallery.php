72f1fe225e6b0a3273dbd755ca5cfe43417c270f
Jorge Fco <jorgefrac@users.noreply.github.com>
2018-06-21 10:09:46 -0500

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,1 +1,5 @@
-
+<?php foreach($page->galeria()->yaml() as $image): ?>
+  <?php if($img = $page->image($image)): ?>
+    <div data-img="<?= $img->url() ?>"></div>
+  <?php endif ?>
+<?php endforeach ?>
