404c2a0dfbe2ab0be80138a21c23182f081ccb56
Jorge Fco <jorgefrac@users.noreply.github.com>
2018-09-17 13:26:54 -0500

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,5 +1,21 @@
+<!--BASIC-->
 <?php foreach($page->galeria()->yaml() as $image): ?>
-  <?php if($img = $page->image($image)): ?>
-    <div data-img="<?= $img->url() ?>"></div>
-  <?php endif ?>
+	<?php if($img = $page->image($image)): ?>
+		<div>
+			<img src="<?= $img->url() ?>" alt="<?= $site->title()->html(); ?>">
+		</div>
+	<?php endif ?>
 <?php endforeach ?>
+<!--BASIC-->
+
+<!--CAROUSEL-->
+<div class="carousel" role="carousel">
+<?php foreach($page->galeria()->yaml() as $image): ?>
+	<?php if($img = $page->image($image)): ?>
+		<div class="carousel__item">
+			<img src="<?= $img->url() ?>" alt="<?= $site->title()->html(); ?>">
+		</div>
+	<?php endif ?>
+<?php endforeach ?>
+</div>
+<!--CAROUSEL-->
