b4d22fc4eab639f71bfbbe39bc29a24a96656420
Jorge Fco <jorgefrac@users.noreply.github.com>
2019-04-16 16:03:10 -0500

    Update categories.php

--- categories.php
+++ categories.php
@@ -7,3 +7,14 @@
   </li>
 <?php endforeach ?>
 </ul>
+
+
+<ul role="filters">
+  <?php foreach ($page->categories()->split(',') as $filter): ?>
+    <li>
+      <a href="#<?= str::slug($filter); ?>" title="<?php echo $filter;  ?>" class="js" role="tag">
+        <?php echo $filter;  ?>
+      </a>
+    </li>
+  <?php endforeach ?>
+</ul>
