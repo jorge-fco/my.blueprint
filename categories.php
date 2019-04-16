bba296e16da0c4f8fbc6b01033eca91529ab0cd3
Jorge Fco <jorgefrac@users.noreply.github.com>
2019-04-16 16:04:06 -0500

    Update categories.php

--- categories.php
+++ categories.php
@@ -1,11 +1,11 @@
 <ul role="tags">
-<?php foreach ($page->categories()->split(',') as $cat): ?>
-	<li>
-	  <a href="#" title="<?php echo $cat;  ?>" class="js" role="tag">
-		  <?php echo $cat;  ?>
-		</a>
-  </li>
-<?php endforeach ?>
+  <?php foreach ($page->categories()->split(',') as $cat): ?>
+    <li>
+      <a href="#" title="<?php echo $cat;  ?>" class="js" role="tag">
+        <?php echo $cat;  ?>
+      </a>
+    </li>
+  <?php endforeach ?>
 </ul>
 
 
